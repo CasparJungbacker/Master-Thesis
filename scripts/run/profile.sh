615722790c0ca67c45ea0a716bbe8ed7f0720645
Caspar Jungbacker <caspar.jungbacker@outlook.com>
2023-07-04 20:13:43 +0200

    Update profiling script
    - Change to Debug version of DALES
    - Use current timestamp in the name of profile file

--- scripts/run/profile.sh
+++ scripts/run/profile.sh
@@ -2,10 +2,10 @@
 
 source $HOME/source_nvidia.sh
 
-DALES_EXEC=$HOME/Developer/build-NV-OpenACC-Release/src/dales4.4
+DALES_EXEC=$HOME/Developer/build-NV-OpenACC-Debug/src/dales4.4
 CASE_DIR=$HOME/my_labdata/test-dales-nv
 NPROCS=1
-FLAGS="-o profile --force-overwrite true"
+FLAGS="-o profile-$(date '+%F_%H:%M:%S') --force-overwrite true"
 
 cd ${CASE_DIR}
 nsys profile ${FLAGS} mpirun -np ${NPROCS} ${DALES_EXEC} ./namoptions.001
