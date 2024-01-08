787cc7c81d0abe7184a960e48db87e05e582bc3f
Caspar Jungbacker <caspar.jungbacker@outlook.com>
2024-01-08 15:41:59 +0100

    Add walltime benchmark files and output

---
+++ benchmarks/walltime/run-gpu.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+for dir in */
+do
+  sbatch \
+    --partition=gpu \
+    --time=00:15:00 \
+    --ntasks=1 \
+    --gpus-per-task=1 \
+    --output=$dir/output_gpu.txt \
+    --export=CASE=$dir \
+    submit-gpu.sh
+done
