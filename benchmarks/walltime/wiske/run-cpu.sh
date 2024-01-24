548182bbe8ac8d2c4ae16f06c827b8fd74644ff4
Caspar Jungbacker <caspar.jungbacker@outlook.com>
2024-01-24 09:18:52 +0100

    Update wiske walltime benchmarks

--- benchmarks/walltime/wiske/run-cpu.sh
+++ benchmarks/walltime/wiske/run-cpu.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+NTASKS=${1:-8}
+
 HERE=~/Developer/mscthesis/benchmarks/walltime/wiske
 TMP_DIR=~/my_scratch/tmp
 
@@ -6,7 +9,7 @@ DALES=~/my_labdata/build-gnu-fast-Release/src/dales4.4
 PROF_INP=~/my_labdata/dales/cases/bomex/prof.inp.001
 LSCALE_INP=~/my_labdata/dales/cases/bomex/lscale.inp.001
 
-for dir in */
+for dir in 1024x0512
 do
   # Copy input files
   CASE_DIR=$(pwd)/$dir
@@ -15,8 +18,8 @@ do
   cp {$PROF_INP,$LSCALE_INP,$CASE_DIR/namoptions.001} .
 
   # Run benchmark
-  mpirun -np 8 $DALES ./namoptions.001 | tee output-cpu.txt
-  cp output-cpu.txt $CASE_DIR
+  mpirun -np $NTASKS $DALES ./namoptions.001 | tee output_cpu_${NTASKS}_tasks.txt
+  cp output_cpu_${NTASKS}_tasks.txt $CASE_DIR
 
   # Clean up
   cd $HERE
