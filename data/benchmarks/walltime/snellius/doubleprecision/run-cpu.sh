8db04309cc5d4cd1e710ab8e8cecc2eee88916be
Caspar Jungbacker <caspar.jungbacker@outlook.com>
2024-02-16 21:23:57 +0100

    Pencils

--- data/benchmarks/walltime/snellius/doubleprecision/run-cpu.sh
+++ data/benchmarks/walltime/snellius/doubleprecision/run-cpu.sh
@@ -6,7 +6,7 @@ do
     --partition=rome \
     --time=00:30:00 \
     --ntasks=128 \
-    --output=$dir/output_cpu.txt \
+    --output=$dir/output_multi_cpu.txt \
     --export=CASE=$dir \
     submit-cpu.sh
 done
